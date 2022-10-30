00842145cd107ae82d5c2b8e600b72db5f81e7ae
Niharika-Sunnam <85931156+Niharika-Sunnam@users.noreply.github.com>
2022-10-30 14:36:10 +0530

    Add files via upload

---
+++ Square root of a number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+   int n;
+   float result;
+   cout<<"Enter number: ";
+   cin>>n;
+   result = pow(n,0.5);
+   cout<<"Square root is "<<result;
+   return 0;
+}
